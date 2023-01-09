c822bf97d7a6049a6af3871d4902ef96339d1b2d
SebastianoUrban <sebastiano.urban98@gmail.com>
2023-01-09 17:16:52 +0100

    created file and first draft of header

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,22 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import DcHeader from './components/DcHeader.vue'
+import DcMain from './components/DcMain.vue'
+import DcFooter from './components/DcFooter.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header>
+    <DcHeader/>
+  </header>
+
+  <main>
+    <DcMain/>
+  </main>
+
+  <footer>
+    <DcFooter/>
+  </footer>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/components/DcHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+    <img src="../assets/img/dc-logo.png" alt="Logo Dc">
+    <ul>
+      <li v-for="navItem in navList">
+        {{ navItem.toUpperCase()}}
+      </li>
+    </ul>
+  </div>
+  
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        navList: ['characters', 'comics', 'movies', 'tv', 'games', 'collectibles', 'videos', 'fans', 'news', 'shop']
+      }
+    },
+  }
+</script>
+
+<style  scoped>
+  div.wrapper{
+    width: 80%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+    padding: 15px 0;
+  }
+
+  ul {
+    list-style-type: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  li {
+    margin-left: 15px;
+    font-weight: bold;
+    font-size: 0.9rem;
+  }
+</style>
